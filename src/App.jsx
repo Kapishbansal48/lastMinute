1a1e9ef330bb4a9dad924a4209fc72952849204a
KAPISH BANSAL <134728855+Kapishbansal48@users.noreply.github.com>
2026-06-30 11:08:04 +0530

    Create App component to render LastMinuteLifeSaver

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import LastMinuteLifeSaver from './last-minute-life-saver'
+
+function App() {
+  return (
+    <LastMinuteLifeSaver />
+  );
+}
+
+export default App
